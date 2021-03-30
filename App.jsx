04d5f8bb8b431db28d2f2db3fddf8dc121f44c56
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-03-30 09:38:38 -0300

    feat: initial commit

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
