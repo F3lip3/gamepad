d04b8aea31e31d5ce361242221910894017d9fa4
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-03-30 11:11:36 -0300

    feat: initial structure

--- App.jsx
+++ App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import GamePad from './src/components/GamePad';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView />
+      <View style={styles.container}>
+        <Text>TESTE</Text>
+        <GamePad />
+        <StatusBar hidden />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center'
   }
---
+++ src/components/GamePad/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const GamePad = () => {
+  return (
+    <View>
+      <Text>GAMEPAD</Text>
+    </View>
+  );
+};
+
+export default GamePad;
